b28554740f4ef741e9aeb12cf232a2a3a6e2b3b4
Rishu jha <jharishu796@gmail.com>
2025-02-19 16:46:15 +0000

    promise polyfil created

---
+++ javascript/polyfils/promise.js
@@ -0,0 +1,64 @@
+function myPromise(executor){
+    let onResolve;
+    let onReject;
+    let fullfield = false;
+    let rejected = false;
+    let result;
+    let called = false;
+
+    function resolveFunc(val){
+        fullfield = true;
+        result = val;
+        if(typeof onResolve === "function"){
+            onResolve(val);
+            console.log("inside resolve");
+            called = true;
+        }
+    }
+
+    function rejectFunc(val){
+        rejected = true;
+        result = val;
+        if(typeof onReject === "function"){
+            onReject(val);
+            called = true;
+        }
+    }
+
+    this.then = function(cb){
+        onResolve = cb;
+        if(fullfield && !called){
+            cb(result);
+            console.log("inside then");
+            called = true;
+        }
+        return this;
+    }
+
+    this.catch = function(cb){
+        onReject = cb;
+        if(rejected && !called){
+            cb(result);
+            called = true;
+        }
+        return this;
+    }
+
+    executor(resolveFunc, rejectFunc);
+}
+
+let temp = new myPromise((res,rej)=>{
+    let x = false;
+    if(x){
+        setTimeout(()=>{
+            res("Promise resolved successfully");
+        },1000);
+    }else{
+        // setTimeout(()=>{
+            rej("Promise rejected");
+        // },1000);
+    }
+})
+
+temp.then((res)=>console.log("res: ", res))
+.catch((err)=>console.log("err: ",err));
